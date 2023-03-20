39f9c716f4d887d27bc2686aa0785ab6ba62423f
Sarewell <libois-nicolas@hotmail.fr>
2023-03-20 14:38:12 +0100

    commit2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 		include ('partial/_header.php');
 		include ('helpers/_data.php');
-		include ('partial\_hero.php');
-		include('partial\home page\_section-salés.php');
-		include('partial\home page\_section-sucrée.php');
-		include('partial\home page\_section-twitch.php');
+		include ('partial/_hero.php');
+		include('partial/home page/_section-salés.php');
+		include('partial/home page/_section-sucrée.php');
+		include('partial/home page/_section-twitch.php');
 		include('partial/_footer.php');
 		?>
